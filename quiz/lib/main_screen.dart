052380fb6f861bdef7b8f82f4b48629fa08832d3
xharf <123190123@student.upnyk.ac.id>
2022-03-15 21:10:56 +0700

    update quiz sampai selesai

--- quiz/lib/main_screen.dart
+++ quiz/lib/main_screen.dart
@@ -11,32 +11,35 @@ class MainScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Spoti-play"),
       ),
-      body: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-          ),
-          itemBuilder: (context, index) {
-            final TopAlbum top_album = topAlbumList[index];
-            return Column(
-              children: [
-                InkWell(
-                  onTap: (){
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context){
-                          return SongList(albumIdx: index);
-                        })
-                    );
-                  },
-                  child:
-                      Card(
-                        child: Image.network(top_album.imageUrls,
-                        width: MediaQuery.of(context).size.width/2-50,),
-                      ),
-                ),
-              ],
-            );
-          },
-        itemCount: topAlbumList.length,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 15.0),
+        child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+            ),
+            itemBuilder: (context, index) {
+              final TopAlbum top_album = topAlbumList[index];
+              return Column(
+                children: [
+                  InkWell(
+                    onTap: (){
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context){
+                            return SongList(albumIdx: index);
+                          })
+                      );
+                    },
+                    child:
+                        Card(
+                          child: Image.network(top_album.imageUrls,
+                          width: MediaQuery.of(context).size.width/2-50,),
+                        ),
+                  ),
+                ],
+              );
+            },
+          itemCount: topAlbumList.length,
+        ),
       )
     );
   }
--- quiz/lib/song_list.dart
+++ quiz/lib/song_list.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'top_album.dart';
 
 class SongList extends StatefulWidget {
   final int albumIdx;
+
   const SongList({Key? key, required this.albumIdx}) : super(key: key);
 
   @override
@@ -11,48 +13,109 @@ class SongList extends StatefulWidget {
 
 class _SongListState extends State<SongList> {
   @override
-  Widget build(BuildContext context) {
+  bool toggle = false;
 
+  Widget build(BuildContext context) {
     final TopAlbum top_album = topAlbumList[widget.albumIdx];
     return Scaffold(
-      appBar: AppBar(
-        title: Text(top_album.albumName),
-      ),
-      body: Container(
-        child: Column(
-          children: [
-            Center(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 16.0),
-                    child: Image.network(top_album.imageUrls),
-                  ),
-                  Text(top_album.albumName,
-                   style: TextStyle(
-                     fontSize: 26,
-                     fontWeight: FontWeight.bold
-                   ),
-                  ),
-                  Text(top_album.singer),
-                  Text("${top_album.releaseDate} | ${top_album.source}"),
-                ],
+        appBar: AppBar(
+          title: Text(top_album.albumName),
+          actions: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: IconButton(
+                icon:
+                    toggle ? Icon(Icons.favorite_border) : Icon(Icons.favorite),
+                onPressed: () {
+                  setState(() {
+                    toggle = !toggle;
+                  });
+                },
               ),
             ),
-            Container(
-              child: ListView.builder(
-                  itemBuilder: (context, index) {
-                    return Card(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        child: Text("${top_album.songs.length}"),
-                      )
-                    );
-                  }, itemCount: top_album.songs.length),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: IconButton(
+                icon: Icon(Icons.share),
+                onPressed: () async {
+                  if (!await launch(top_album.albumurl)) throw 'Could not launch ${top_album.albumurl}';
+                }
+              ),
             )
           ],
-        )
-      )
-    );
+        ),
+        body: ListView(children: [
+          Column(
+            children: [
+              Center(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: Image.network(top_album.imageUrls),
+                    ),
+                    Text(
+                      top_album.albumName,
+                      style:
+                          TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+                    ),
+                    Text(top_album.singer),
+                    Text("${top_album.releaseDate} | ${top_album.source}"),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Card(
+                color: Colors.grey,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      ListTile(
+                        leading: Text(
+                          "# |",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        title: Text(
+                          "Song List",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15),
+                child: SingleChildScrollView(
+                    child: SizedBox(height: 400, child: _build_songlist())),
+              )
+            ],
+          )
+        ]));
+  }
+
+  Widget _build_songlist() {
+    TopAlbum top_album = topAlbumList[widget.albumIdx];
+    return ListView.builder(
+        itemBuilder: (context, index) {
+          return InkWell(
+            child: Card(
+                child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: Text("${index + 1} |"),
+                    title: Text(top_album.songs[index]),
+                  )
+                ],
+              ),
+            )),
+          );
+        },
+        itemCount: top_album.songs.length);
   }
 }
